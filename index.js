b14c892d6ca9ab27148c3d5829e0bb2f8523db06
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-07-31 10:58:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(friends, holiday) {
+    let cards = []
+    for (let x = 0; x < friends.length; x++) {
+      cards.push(`Thank you, ${friends[x]}, for the wonderful ${holiday} gift!`);
+    }
+    return cards
+}
+
+function countDown(num) {
+    while (num >= 0 ) {
+        console.log(num)
+        num--;
+    } 
+}
